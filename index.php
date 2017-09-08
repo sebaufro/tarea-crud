cd75e864b31b3944e1efaaf8f8f3aa311dff0041
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-09-07 23:42:02 -0300

    Actualizar index y validar.php - Cambiar bootstrap a versión 4 - Cambiar jquery a versión 3.algo - Comentar validar.php - Actualizar index para usar bootstrap 4

--- index.php
+++ index.php
@@ -1,56 +1,48 @@
-
 <?php 
-	$nombre = "Juan Nieve"; 
+session_start();
+$error = '';
+if(isset($_SESSION['error'])) {
+  $error = $_SESSION['error'];
+  unset($_SESSION['error']);
+}
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<title>Login</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/estilo.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Iniciar sesión</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-	<div class="container-fluid">
-		<div class="panel panel-info col-xs-12 col-sm-4 col-sm-offset-4">
-			<div class="panel-heading">
-				<h3 class="panel-title">Login</h3>
-			</div>
-			<div class="panel-body">
-
-				<form class="form-horizontal" action="validar.php" method="POST">
-				  <div class="form-group">
-				    <label for="user" class="col-sm-2 control-label">Usuario</label>
-				    <div class="col-sm-10">
-				      <input type="user" name="user" class="form-control" id="user" placeholder="Usuario">
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label for="passwd" class="col-sm-2 control-label">Password</label>
-				    <div class="col-sm-10">
-				      <input type="password" name="passwd" class="form-control" id="passwd" placeholder="Contraseña">
-				    </div>
-				  </div>
-				  
-				  <div class="form-group">
-				      <button type="submit" class="btn btn-primary btn-block">Login</button>
-				  </div>
-				</form>
-				<?php 
-					if(isset($_GET["error"])){
-						if ($_GET["error"] = "err1") {
-							echo "<div class=\"alert alert-danger\">
-									<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>
-									<strong>ERROR!</strong> El usuario o contraseña son incorrectos.
-								</div>";
-						}
-					}
-				 ?>
-			</div>
-		</div>
-	</div>
-	<script src="js/jquery-1.12.3.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/script.min.js"></script>
+  <div class="container">
+    <div class="row justify-content-md-center mt-5">
+      <div class="col-sm-6">
+        <div class="card">
+          <h4 class="card-header">Iniciar sesión</h4>
+          <div class="card-body">
+            <form action="validar.php" method="post">
+              <div class="form-group">
+                <label for="usuario">Usuario</label>
+                <input type="text" name="usuario" class="form-control" id="usuario">
+              </div>
+              <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input type="password" name="passwd" class="form-control" id="password">
+              </div>
+              <button type="submit" class="btn btn-primary">Ingresar</button>
+            </form>
+          </div>
+        </div>
+        <?php if($error) { ?>
+        <div class="alert alert-danger mt-3" role="alert">
+          <b>Error</b>: <?php echo $error; ?>
+        </div>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
