18cbdaf60a1aa60878294f2bb984d931b97ff99a
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-09-07 22:50:52 -0300

    Sanitizar valores en crear-noticia

--- crear-noticia.php
+++ crear-noticia.php
@@ -1,15 +1,16 @@
 <?php 
-	include 'db.php';
-	session_start();
 
-	if(isset($_POST["titulo"]) && isset($_POST["texto"]) && $_SESSION["rol"]== 1){
-		$pdo->query("INSERT INTO noticias (titulo, texto)
-			VALUES ('".$_POST["titulo"]."',
-					'".$_POST["texto"]."');");
+include 'db.php';
+session_start();
 
-		
-		$datos = array('ok' => '1', 'enlace' => $_SERVER[HTTP_HOST]);
+if(isset($_POST["titulo"]) && isset($_POST["texto"]) && $_SESSION["rol"] == 1){
+    $titulo = $db->escape_string($_POST['titulo']);
+    $texto = $db->escape_string($_POST['texto']);
 
-		echo json_encode($datos);
-	}
- ?>
+    $db->query("INSERT INTO noticias (titulo, texto) " .
+        "VALUES ('".$titulo."', '".$texto."');");
+
+    
+    $datos = array('ok' => '1');
+    echo json_encode($datos);
+}
